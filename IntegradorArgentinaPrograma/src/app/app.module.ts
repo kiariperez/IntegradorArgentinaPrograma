c28ff2923257d95e94641d555b5cedb164e48575
kiariperez <perezkiara@hotmail.com>
2023-04-18 14:23:34 -0300

    otros cambios

--- IntegradorArgentinaPrograma/src/app/app.module.ts
+++ IntegradorArgentinaPrograma/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoArgentinaComponent } from './components/logo-argentina/logo-argenti
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoArgentinaComponent,
     RedesComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
